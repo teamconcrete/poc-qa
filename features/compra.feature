0d354b6ca15ccad97de40d2fcad3661218bb6d46
Cássio Alves <cassio.alves@concrete.com.br>
2017-12-07 15:52:01 -0200

    create the project structure

---
+++ features/compra.feature
@@ -0,0 +1,8 @@
+# language: pt
+Funcionalidade: Compra 
+
+    
+  Cenário: Realizar uma compra
+  Dado que estou na tela inicial
+  Quando seleciono o produto
+  Então a compra deve ser realizada
---
+++ features/ios/screens/compra_screen.rb
@@ -0,0 +1,6 @@
+class CompraScreen < BaseScreen
+
+# Declare all the elements of this screen
+  # identificator(:trait)               { pending 'Insert button identificator' }
+  # identificator(:button)              { pending 'Insert button identificator' }
+end
